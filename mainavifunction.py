07148a94174366eb9860adda0336974d8c80082e
AVI TOMAR <78687121+tomaravi7@users.noreply.github.com>
2022-02-06 19:02:40 +0530

    commented errors

--- mainavifunction.py
+++ mainavifunction.py
@@ -3,9 +3,8 @@ from matplotlib.pyplot import close
 from moviepy.editor import *
 from moviepy.editor import VideoFileClip
 import tkinter
-from tkinter import ttk
-
 from pyparsing import col;
+
 def removeaudio():
     file=input("Plese enter the complete path of the video file you want to enter : ");
     dec=input("Do you want to overwrite the file or create a new file(write OVERWRITE or NEW) : ");
@@ -30,15 +29,13 @@ def getaudio():
 win=tkinter.Tk(screenName='AV operations')
 win.title("Audio Video Operations")
 win.geometry("520x720")
-win.iconphoto(False,tkinter.PhotoImage(file="logo.jpg")) #giving error sometimes and working sometimes
+# win.iconphoto(False,tkinter.PhotoImage(file="logo.jpg")); #giving error sometimes and working sometimes
 #removeaudio
 tkinter.Label(win,text='Audio Remover ',font=('Arial',22),fg="#0000FF", pady= 20,padx=10).grid(row=0,sticky='w')
 tkinter.Label(win,text='Enter the address of the video ',padx=10).grid(row=1,column=0,sticky='w')
 file=tkinter.Entry(win,).grid(row=1,column=1)
 b=tkinter.Button(win,text='Remove Audio',bg="cyan",command=removeaudio,padx=10).grid(row=2,column=0,sticky='w') #add checkbox to ask if user wants to overwrite the old file or make a new file
 
-separator = ttk.Separator(win, orient='horizontal')
-separator.place(relx=0.47, rely=0, relwidth=0.2, relheight=1)
 
 #remove video and get mp3
 tkinter.Label(win,text='Video Remover ',font=('Arial',22),fg="#5D1049",pady= 20,padx=10).grid(row=3,sticky='w')
